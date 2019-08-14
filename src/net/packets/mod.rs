c0908178b5b3bc1a0cc6177dbda207e9d0bb03d0
Moritz Gunz <moritz.gunz@gmail.com>
2019-08-14 15:17:58 +0200

    :sparkles: Implement getter for packet variants

--- src/net/packets/mod.rs
+++ src/net/packets/mod.rs
@@ -37,3 +37,27 @@ pub trait Outgoing {
 
     fn write_to(&self, dst: &mut BytesMut) -> Result<(), io::Error>;
 }
+
+macro_rules! into_getter {
+    ($($name:ident -> $variant:ident),+) => {
+        $(
+            pub fn $name(self) -> Result<$variant, Self> {
+                match self {
+                    Self::$variant(x) => Ok(x),
+                    _ => Err(self)
+                }
+            }
+        )*
+    };
+}
+
+// TODO: Generate this using a proc macro
+impl IncomingPackets {
+    into_getter!(
+        into_encryption_response -> EncryptionResponse,
+        into_handshake -> Handshake,
+        into_login_start -> LoginStart,
+        into_ping -> Ping,
+        into_status_handshake -> StatusHandshake
+    );
+}
